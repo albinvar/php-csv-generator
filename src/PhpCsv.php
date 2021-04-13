1ecd77fc4b850f5dd1bb3a8e6ae4054732d1a6f5
Albin Varghese <albin8285@gmail.com>
2021-04-13 20:14:55 +0530

    add setArray and setCsv methods

--- src/PhpCsv.php
+++ src/PhpCsv.php
@@ -2,85 +2,26 @@
 
 namespace App;
 
-class PhpCsv {
+class PhpCsv
+{
 	
-	private $file;
-	private $data;
-	public $resultArray;
-	
-	public function setCsv($file) {
-		
-		$data = file_get_contents($file);
-		$this->file = $file;
-		$this->data = $data;
-		
-		}
-		
-	public function setArray($array) {
-		
-	$this->setArray = $array;
-	
-		
-		}
-		
-	public function createArray() {
-		
-		$string = $this->data;
-		$delimiter=',';
-		$header = NULL;
-        $data = [];
-        $rows = explode(PHP_EOL, $string); 
-        $header = $rows[0];//creates a copy of csv header array
-unset($rows[0]);//removes the header from $csv_data since no longer needed
-foreach($rows as $row){
-	var_dump($header);
-	var_dump($row);
-    $row = array_combine($header, $row);// adds header to each row as key
-    var_dump($row);//do something here with each row
-}
-//foreach($records as $test) {
-            //var_dump($element);
-            //$test[] = array_combine($header, $element);
-
-//}
-
-        
-		}
-	
-	public function createCsv($array) {
-	
-	
-	$pathToGenerate = 'array.csv'; 
-    $header=null;
-    $createFile = fopen($pathToGenerate,"w+");
-	 
-    foreach ($array as $row) {
-
-        if(!$header) {
-
-            fputcsv($createFile,array_keys($row));
-            fputcsv($createFile, $row);  
-            $header = true;
-        }
-        else {
-
-            fputcsv($createFile, $row);
-        }
-    }
-    fclose($createFile);
-	echo "success";
+	public function __construct()
+	{
+		//
 	}
 	
-	public function getArray() {
+	public function setCsv($file)
+	{
+		$data = file_get_contents($file);
 		
-		return $this->createArray();
-		
-		}
-		
-	public function getCsv($array) {
-		
-		return $this->createCsv($array);
-		
-		}
+		$this->file = $file;
+		$this->data = $data;
+	}
 	
-	};
+	
+	public function setArray($array)
+	{
+		$this->setArray = $array;
+	}
+	
+}
