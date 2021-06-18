ed627e42f8dc8e04f07bfeb219d98f18b3a3f4e8
Albin Varghese <albin8285@gmail.com>
2021-06-18 18:21:51 +0530

    basic fixes

--- tests/GenerateJsonTest.php
+++ tests/GenerateJsonTest.php
@@ -10,15 +10,14 @@ class GenerateJsonTest extends TestCase
 {
 	public function test_convert_array_to_json()
 	{
-		$array = [
-			['Name', 'Age'],
+		$columns = ['Name', 'Age'];
+		$array = [ 
 			['John', 28],
 			['Johana', 23],
 			['Adam', 32],
 		];
-		
 		$object = new Generator();
-		$object->setArray($array);
+		$object->setArray($array, $columns);
 		
 		$jsonString = $object->exportJson();
 		
