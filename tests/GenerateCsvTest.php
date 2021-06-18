0b6ccd9033baa36aaf3a511e0643d06d98ac7688
Albin Varghese <albin8285@gmail.com>
2021-06-18 18:24:47 +0530

    php-cs-fix

--- tests/GenerateCsvTest.php
+++ tests/GenerateCsvTest.php
@@ -8,21 +8,20 @@ require_once("./vendor/autoload.php");
 
 class GenerateCsvTest extends TestCase
 {
-	public function test_convert_array_to_csv()
-	{
-		
-		$columns = ['Name', 'Age'];
-		$array = [ 
-			['John', 28],
-			['Johana', 23],
-			['Adam', 32],
-		];
-		$object = new Generator();
-		$object->setArray($array, $columns);
-		$object->makeCsv();
-		
-		$expectedOutput = "Name,Age\nJohn,28\nJohana,23\nAdam,32";
-		
-		return $this->assertTrue(strpos($object->getCsv(),$expectedOutput) !== false);
-	}
+    public function test_convert_array_to_csv()
+    {
+        $columns = ['Name', 'Age'];
+        $array = [
+            ['John', 28],
+            ['Johana', 23],
+            ['Adam', 32],
+        ];
+        $object = new Generator();
+        $object->setArray($array, $columns);
+        $object->makeCsv();
+        
+        $expectedOutput = "Name,Age\nJohn,28\nJohana,23\nAdam,32";
+        
+        return $this->assertTrue(strpos($object->getCsv(), $expectedOutput) !== false);
+    }
 }
--- tests/GenerateJsonTest.php
+++ tests/GenerateJsonTest.php
@@ -8,19 +8,19 @@ require_once("./vendor/autoload.php");
 
 class GenerateJsonTest extends TestCase
 {
-	public function test_convert_array_to_json()
-	{
-		$columns = ['Name', 'Age'];
-		$array = [ 
-			['John', 28],
-			['Johana', 23],
-			['Adam', 32],
-		];
-		$object = new Generator();
-		$object->setArray($array, $columns);
-		
-		$jsonString = $object->exportJson();
-		
-		return $this->assertTrue(!is_null(json_decode($jsonString)));
-	}
+    public function test_convert_array_to_json()
+    {
+        $columns = ['Name', 'Age'];
+        $array = [
+            ['John', 28],
+            ['Johana', 23],
+            ['Adam', 32],
+        ];
+        $object = new Generator();
+        $object->setArray($array, $columns);
+        
+        $jsonString = $object->exportJson();
+        
+        return $this->assertTrue(!is_null(json_decode($jsonString)));
+    }
 }
